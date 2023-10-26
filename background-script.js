6ca3556500a1e0f232ca28c0aefcb0dd20e738ac
Jatin Dua <jdua004@gmail.com>
2023-10-26 17:05:26 +0530

    feat: Add basic message ports

---
+++ background-script.js
@@ -0,0 +1,19 @@
+// background-script.js
+
+let portFromCS;
+
+function connected(p) {
+  portFromCS = p;
+  portFromCS.postMessage({ greeting: "hi there content script!" });
+  portFromCS.onMessage.addListener((m) => {
+    portFromCS.postMessage({
+      greeting: `In background script, received message from content script: ${m.greeting}`,
+    });
+  });
+}
+
+browser.runtime.onConnect.addListener(connected);
+
+browser.browserAction.onClicked.addListener(() => {
+  portFromCS.postMessage({ greeting: "they clicked the button!" });
+});
---
+++ mindfulscroll.js
@@ -0,0 +1,13 @@
+// content-script.js
+
+let myPort = browser.runtime.connect({ name: "port-from-cs" });
+myPort.postMessage({ greeting: "hello from content script" });
+
+myPort.onMessage.addListener((m) => {
+  console.log("In content script, received message from background script: ");
+  console.log(m.greeting);
+});
+
+document.body.addEventListener("click", () => {
+  myPort.postMessage({ greeting: "they clicked the page!" });
+});
